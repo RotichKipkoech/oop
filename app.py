a84f7c22fd3e6f8bee88138580bd56c1cc49957a
Kennedy Rotich <132645931+RotichKipkoech@users.noreply.github.com>
2024-12-11 13:00:01 +0300

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ class Person:
         print(f"Hello! My name is {self.name}. I am {self.age} years old and I am {self.gender}.")
 
 # Create an instance of the Person class
-person1 = Person("Alice", 30, "Female")
+person1 = Person("Kenedy Kipkoech Rotich", 31, "Male")
 
 # Call the introduce method to display the person's information
 person1.introduce()
